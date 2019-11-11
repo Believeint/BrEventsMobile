5d9b3efbbbd4ee0e07f1510e12052627ae228f7b
elias.santos <elias.carvalho.096@gmail.com>
2019-11-11 17:30:58 -0300

    Path Banco

--- BrEvents/BrEvents/App.xaml.cs
+++ BrEvents/BrEvents/App.xaml.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using BrEvents.Models;
+using System.IO;
 
 namespace BrEvents
 {
     public partial class App : Application
     {
+        static Banco banco;
+        public static Banco DB
+        {
+            get
+            {
+                if(banco == null)
+                {
+                    banco = new Banco(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco.db3"));
+                }
+                return banco;
+            }
+        }
         public App()
         {
             InitializeComponent();
